aa1b591c986548f69446754a5307e8dac0f3778b
PerryXDeng <pxd256@protonmail.com>
2019-06-16 13:25:41 -0400

    transformation and visualization

---
+++ 2/utility.R
@@ -0,0 +1,43 @@
+library(readr)
+library(tidyr)
+
+START_YEAR = 2010
+END_YEAR = 2017
+YEAR_SEQUENCE = seq(as.Date("2010/7/1"), as.Date("2017/7/1"), "years")
+
+HW_DIR <- "./2/"
+DATA_DIR <- paste(HW_DIR, "datasets/", sep="")
+
+load_data <- function(){
+  return(read_csv(paste(DATA_DIR, "metro_and_micro_areas/cbsa-est2018-alldata.csv",sep="")))
+}
+
+load_combined_areas_data <- function(){
+  return(read_csv(paste(DATA_DIR, "combined_areas/csa-est2018-alldata.csv",sep="")))
+}
+
+get_yearly_column_names <- function(attribute_name){
+  names <- c()
+  for (year in START_YEAR:END_YEAR){
+    yearly_name = paste(attribute_name, year, sep="")
+    names <- c(names, yearly_name)
+  }
+  return(names)
+}
+
+transposed_time_indexed_dataframe <- function(df, attribute_name){
+  # new dataframe becomes tuples of (time, location, value)
+  # inverts the original dataframe and index observations by year instead of locations
+  # easier to scatter plot than original dataframe
+  
+  yearly_attribute_names = get_yearly_column_names(attribute_name)
+  transformed = as.data.frame(t(df[yearly_attribute_names]))
+  # column names become location names
+  names(transformed) = unlist(df["NAME"])
+  # add date column
+  transformed = cbind(date = YEAR_SEQUENCE, transformed)
+  # turn location into a variable with its own column
+  # similar to pandas.dataframe.melt()
+  transformed = gather(transformed, key = "location", value = attribute_name, -date)
+  return(transformed)
+}
---
+++ 2/visualization.R
@@ -0,0 +1,26 @@
+library(ggplot2)
+source("utility.R")
+
+
+time_series_scatterplots <- function(df, attribute_name, number_of_locations){
+  # plots over lapping scatter plots of attribute_name over years
+  # each location is a series
+  # randomly chooses number_of_locations areas from dataset
+  df = df[sample(nrow(df), number_of_locations), ]
+  df = transposed_time_indexed_dataframe(df, attribute_name)
+  p = ggplot(df, aes(x=date, y=attribute_name)) +
+      geom_point(aes(color=location)) +
+      geom_line(aes(color=location), size=1) +
+      ylab(attribute_name) +
+      theme_minimal()
+  plot(p)
+}
+
+
+main <- function(){
+  combined_data = load_combined_areas_data()
+  time_series_scatterplots(combined_data, "POPESTIMATE", 30)
+}
+
+
+main()
