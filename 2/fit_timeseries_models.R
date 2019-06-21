7eaa72a15ee409e4725954ac89f572ab63eeeb88
PerryXDeng <pxd256@protonmail.com>
2019-06-21 13:26:50 -0400

    modeling

--- 2/fit_timeseries_models.R
+++ 2/fit_timeseries_models.R
@@ -1,12 +1,21 @@
-source("./2/utlity.R")
+source("./2/utility.R")
 source("./2/holt.R")
 
+STEPS_PREDICTED = 3
+STEPS_SMOOTHED = END_YEAR - START_YEAR - STEPS_PREDICTED
+
+holt_on_location <- function(mat, location_index, typ="additive", damp=FALSE, a=0.2, b=0.1057){
+  smoothed_vector = unlist(mat[seq(1:1 + STEPS_SMOOTHED), 1532])
+  forecasted_vector = unlist(mat[seq(2 + STEPS_SMOOTHED:2 + STEPS_SMOOTHED + STEPS_PREDICTED), 1532])
+  
+  model = Holt(smoothed_vector, type=typ, damped=damp, alpha=a, beta=b, lead=STEPS_PREDICTED)
+  smoothed_mape = model$accurate["MAPE"]
+  smoothed_mpe = model$accurate["MPE"]
+  forecast_metrics = accurate(forecasted_vector, model$pred, 2, output=FALSE)
+  forecasted_mape = forecast_metrics["MAPE"]
+  forecasted_mpe = forecast_metrics["MPE"]
+}
+
 df = load_combined_areas_data()
-
 mat = transposed_time_series_matrix(df, "POPESTIMATE")
-
-selected_vector = unlist(mat[1532])
-Holt(selected_vector)
-Holt(selected_vector, type="multiplicative")
-names = get_location_names(df)
-print(names[1532])
+holt_on_location(mat, 1532)
