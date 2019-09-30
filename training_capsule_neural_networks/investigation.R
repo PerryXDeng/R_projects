ee57f0c3db9ad0897b350ce53806b3098d4d7d73
PerryXDeng <pxd256@protonmail.com>
2019-09-29 22:36:19 -0400

    tests and reports

--- training_capsule_neural_networks/investigation.R
+++ training_capsule_neural_networks/investigation.R
@@ -19,21 +19,96 @@ one_boxplot_10_epochs_accuracy <- function(df, attribute_name, architecture){
 
 ten_density_plots_10_epochs <- function(df, attribute_name, architecture){
   for (ep in unique(df$epoch)){
-    titl = sprintf("distribution of %s, epoch: %d, architecture: %s", attribute_name, ep, architecture)
+    titl = sprintf("distribution of %s, epoch: %s, architecture: %s", attribute_name, ep, architecture)
     data = df[df$epoch == ep,]
-    p = ggplot(data, aes_string(y=attribute_name)) + geom_density() + ggtitle(titl)
+    p = ggplot(data, aes_string(x=attribute_name)) + geom_density() + ggtitle(titl)
     print(p)
   }
 }
 
-main <- function(){
-  architecture = "smaller"
+ten_observations <- function(architecture, attribute_name){
   target_file = sprintf("./%s/%s_subset.csv", architecture, architecture)
-  #attribute_name = "hours_elapsed"
-  attribute_name = "hours_elapsed"
   df = read_csv(target_file)
   df$epoch <- as.factor(df$epoch)
-  one_boxplot_10_epochs(df, attribute_name, architecture)
+  ten_density_plots_10_epochs(df, attribute_name, architecture)
+}
+
+hundred_observations <- function(attribute_name){
+  affine_file = sprintf("./%s/%s_mins.csv", "affine", "affine")
+  smaller_file = sprintf("./%s/%s_mins.csv", "smaller", "smaller")
+  affine_df = read_csv(affine_file)
+  smaller_df = read_csv(smaller_file)
+  affine_df$architecture = rep("affine", nrow(affine_df))
+  smaller_df$architecture = rep("original", nrow(smaller_df))
+  union = rbind(affine_df, smaller_df)
+  titl = sprintf("mins of %s over time, two samples of size 10 over 100 epochs", attribute_name)
+  p = ggplot(union, aes_string(x="epoch", y=attribute_name)) + geom_line(aes(color=architecture)) + ggtitle(titl)
+  print(p)
+}
+
+differences_in_mean_observations <- function(){
+  file = "./affine_minus_smaller_accuracies.csv"
+  df = read_csv(file)
+  
+  titl = sprintf("differences in mean accuracies over time (affine - original), two samples of size 10 over 100 epochs")
+  p = ggplot(df, aes(x=epoch, y=validation_accuracy)) + geom_line() + ggtitle(titl) + scale_y_continuous(limits=c(-0.04, 0.04))
+  print(p)
+  
+  explaination = "sw_p is p value for shapiro wilk test against normality; t_p is p value for right tailed one sample t test against zero mean"
+  
+  shapiro_p = shapiro.test(df$validation_accuracy)$p.value
+  t_p = t.test(df$validation_accuracy, mu=0, alternative="greater")$p.value
+  titl = sprintf("distribution of differences in mean accuracies (affine - original), sw_p=%.3f, t_p=%.3f",shapiro_p, t_p)
+  p = ggplot(df, aes(x=validation_accuracy)) + geom_density() + ggtitle(titl) + labs(caption=explaination)
+  print(p)
+  
+  df = subset(df, epoch>30)
+  shapiro_p = shapiro.test(df$validation_accuracy)$p.value
+  t_p = t.test(df$validation_accuracy, mu=0, alternative="greater")$p.value
+  titl = sprintf("distribution of differences in mean accuracies (affine - original), epoch>30, sw_p=%.3f, t_p=%.3f",shapiro_p, t_p)
+  p = ggplot(df, aes(x=validation_accuracy)) + geom_density() + ggtitle(titl) + labs(caption=explaination)
+  print(p)
+  
+  df = subset(df, epoch>70)
+  shapiro_p = shapiro.test(df$validation_accuracy)$p.value
+  t_p = t.test(df$validation_accuracy, mu=0, alternative="greater")$p.value
+  titl = sprintf("distribution of differences in mean accuracies (affine - original), epoch>70, sw_p=%.3f, t_p=%.3f",shapiro_p, t_p)
+  p = ggplot(df, aes(x=validation_accuracy)) + geom_density() + ggtitle(titl) + labs(caption=explaination)
+  print(p)
+  
+  explaination = "sw_p is p value for shapiro wilk test against normality; t_p is p value for right tailed one sample t test against 0.2% mean"
+  df = read_csv(file)
+  shapiro_p = shapiro.test(df$validation_accuracy)$p.value
+  t_p = t.test(df$validation_accuracy, mu=0.002, alternative="greater")$p.value
+  titl = sprintf("distribution of differences in mean accuracies (affine - original), sw_p=%.3f, t_p=%.3f",shapiro_p, t_p)
+  p = ggplot(df, aes(x=validation_accuracy)) + geom_density() + ggtitle(titl) + labs(caption=explaination)
+  print(p)
+  
+  df = subset(df, epoch>30)
+  shapiro_p = shapiro.test(df$validation_accuracy)$p.value
+  t_p = t.test(df$validation_accuracy, mu=0.002, alternative="greater")$p.value
+  titl = sprintf("distribution of differences in mean accuracies (affine - original), epoch>30, sw_p=%.3f, t_p=%.3f",shapiro_p, t_p)
+  p = ggplot(df, aes(x=validation_accuracy)) + geom_density() + ggtitle(titl) + labs(caption=explaination)
+  print(p)
+  
+  df = subset(df, epoch>70)
+  shapiro_p = shapiro.test(df$validation_accuracy)$p.value
+  t_p = t.test(df$validation_accuracy, mu=0.002, alternative="greater")$p.value
+  titl = sprintf("distribution of differences in mean accuracies (affine - original), epoch>70, sw_p=%.3f, t_p=%.3f",shapiro_p, t_p)
+  p = ggplot(df, aes(x=validation_accuracy)) + geom_density() + ggtitle(titl) + labs(caption=explaination)
+  print(p)
+}
+
+main <- function(){
+  #attribute_name = "hours_elapsed"
+  attribute_name = "validation_accuracy"
+  
+  #architecture = "smaller"
+  #architecture = "affine"
+  #ten_observations(architecture, attribute_name)
+  
+  #hundred_observations(attribute_name)
+  differences_in_mean_observations()
 }
 
 if (sys.nframe() == 0) {
--- training_capsule_neural_networks/preprocess.py
+++ training_capsule_neural_networks/preprocess.py
@@ -7,9 +7,9 @@ def index_epoch_and_offset_time():
     df = pd.read_csv(fil, index_col=False, header=0)
     df = df[0:101]
     df = df.drop(columns=['Step'])
-    df['hours elapsed'] = (df['Wall time'] - df['Wall time'][0])/3600
+    df['hours_elapsed'] = (df['Wall time'] - df['Wall time'][0])/3600
     df = df.drop(columns=['Wall time'])
-    df = df.rename(columns={'Value':'validation accuracy'})
+    df = df.rename(columns={'Value':'validation_accuracy'})
     df.to_csv(fil, index_label="epoch")
   return
 
@@ -17,7 +17,7 @@ def fix_errors():
   files = glob.glob("./*/*.csv")
   for fil in files:
     df = pd.read_csv(fil)
-    df = df.rename(columns={'Value':'validation accuracy'})
+    df = df.rename(columns={'Value':'validation_accuracy'})
     df.to_csv(fil,index=False)
 
 def union_subsets():
@@ -33,8 +33,42 @@ def subset_observations():
   df = df.loc[df["epoch"] % 10 == 0]
   df.to_csv("./smaller/smaller_subset.csv", index=False)
 
+def median_values():
+  files = glob.glob("./*/*_union.csv")
+  for fil in files:
+    df = pd.read_csv(fil)
+    df = df.groupby(['epoch']).median()
+    df.to_csv(fil[0:-9] + "medians.csv")
+
+def min_values():
+  files = glob.glob("./*/*_union.csv")
+  for fil in files:
+    df = pd.read_csv(fil)
+    df = df.groupby(['epoch']).min()
+    df.to_csv(fil[0:-9] + "mins.csv")
+
+def max_values():
+  files = glob.glob("./*/*_union.csv")
+  for fil in files:
+    df = pd.read_csv(fil)
+    df = df.groupby(['epoch']).max()
+    df.to_csv(fil[0:-9] + "maxs.csv")
+
+def mean_values():
+  files = glob.glob("./*/*_union.csv")
+  for fil in files:
+    df = pd.read_csv(fil)
+    df = df.groupby(['epoch']).max()
+    df.to_csv(fil[0:-9] + "means.csv")
+
+def differences_in_accuracies(): 
+  smaller_means = pd.read_csv("./smaller/smaller_means.csv")
+  affine_means = pd.read_csv("./affine/affine_means.csv")
+  df = affine_means.set_index("epoch").subtract(smaller_means.set_index("epoch")).drop(columns=["hours_elapsed"])
+  df.to_csv("./affine_minus_smaller_accuracies.csv")
+
 def main():
-  subset_observations()
+  differences_in_accuracies()
 
 if __name__ == "__main__":
   main()
