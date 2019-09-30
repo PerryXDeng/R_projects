533d410cf77184f2ac30aafbe828c7ce5881b158
PerryXDeng <pxd256@protonmail.com>
2019-09-29 23:28:59 -0400

    refactored dirs

--- training_capsule_neural_networks/preprocess.py
+++ training_capsule_neural_networks/preprocess.py
@@ -2,7 +2,7 @@ import pandas as pd
 import glob
 
 def index_epoch_and_offset_time():
-  files = glob.glob("./*/*.csv")
+  files = glob.glob("./400_epochs/*/*.csv")
   for fil in files:
     df = pd.read_csv(fil, index_col=False, header=0)
     df = df[0:101]
@@ -13,61 +13,74 @@ def index_epoch_and_offset_time():
     df.to_csv(fil, index_label="epoch")
   return
 
-def fix_errors():
-  files = glob.glob("./*/*.csv")
-  for fil in files:
-    df = pd.read_csv(fil)
-    df = df.rename(columns={'Value':'validation_accuracy'})
-    df.to_csv(fil,index=False)
-
 def union_subsets():
-  files = glob.glob("./smaller/*.csv")
+  parent = "./400_epochs/"
+  files = glob.glob(parent + "smaller/*.csv")
   dfs = []
   for fil in files:
     dfs.append(pd.read_csv(fil))
   union = pd.concat(dfs)
-  union.to_csv("./smaller/smaller_union.csv",index=False)
+  union.to_csv(parent + "smaller/smaller_union.csv",index=False)
+  
+  files = glob.glob(parent + "affine/*.csv")
+  dfs = []
+  for fil in files:
+    dfs.append(pd.read_csv(fil))
+  union = pd.concat(dfs)
+  union.to_csv(parent + "affine/affine_union.csv",index=False)
+
 
 def subset_observations():
-  df = pd.read_csv("./smaller/smaller_union.csv")
+  parent = "./400_epochs/"
+  df = pd.read_csv(parent + "smaller/smaller_union.csv")
   df = df.loc[df["epoch"] % 10 == 0]
-  df.to_csv("./smaller/smaller_subset.csv", index=False)
+  df.to_csv(parent + "smaller/smaller_subset.csv", index=False)
+  df = pd.read_csv(parent + "affine/affine_union.csv")
+  df = df.loc[df["epoch"] % 10 == 0]
+  df.to_csv(parent + "affine/affine_subset.csv", index=False)
+
 
 def median_values():
-  files = glob.glob("./*/*_union.csv")
+  files = glob.glob("./*/*/*_union.csv")
   for fil in files:
     df = pd.read_csv(fil)
     df = df.groupby(['epoch']).median()
     df.to_csv(fil[0:-9] + "medians.csv")
 
 def min_values():
-  files = glob.glob("./*/*_union.csv")
+  files = glob.glob("./*/*/*_union.csv")
   for fil in files:
     df = pd.read_csv(fil)
     df = df.groupby(['epoch']).min()
     df.to_csv(fil[0:-9] + "mins.csv")
 
 def max_values():
-  files = glob.glob("./*/*_union.csv")
+  files = glob.glob("./*/*/*_union.csv")
   for fil in files:
     df = pd.read_csv(fil)
     df = df.groupby(['epoch']).max()
     df.to_csv(fil[0:-9] + "maxs.csv")
 
 def mean_values():
-  files = glob.glob("./*/*_union.csv")
+  files = glob.glob("./*/*/*_union.csv")
   for fil in files:
     df = pd.read_csv(fil)
     df = df.groupby(['epoch']).max()
     df.to_csv(fil[0:-9] + "means.csv")
 
 def differences_in_accuracies(): 
-  smaller_means = pd.read_csv("./smaller/smaller_means.csv")
-  affine_means = pd.read_csv("./affine/affine_means.csv")
+  parent = "./400_epochs/"
+  smaller_means = pd.read_csv(parent + "smaller/smaller_means.csv")
+  affine_means = pd.read_csv(parent + "affine/affine_means.csv")
   df = affine_means.set_index("epoch").subtract(smaller_means.set_index("epoch")).drop(columns=["hours_elapsed"])
-  df.to_csv("./affine_minus_smaller_accuracies.csv")
+  df.to_csv(parent + "affine_minus_smaller_accuracies.csv")
 
 def main():
+  index_epoch_and_offset_time()
+  union_subsets()
+  median_values()
+  mean_values()
+  min_values()
   differences_in_accuracies()
 
 if __name__ == "__main__":
